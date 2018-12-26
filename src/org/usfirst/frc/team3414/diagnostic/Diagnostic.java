ed08890085810d87d1f390c7780ff56ff92cea54
Jon <>
2018-12-26 08:58:37 -0500

    added reset code for Diagnostic

--- src/org/usfirst/frc/team3414/diagnostic/Diagnostic.java
+++ src/org/usfirst/frc/team3414/diagnostic/Diagnostic.java
@@ -5,15 +5,17 @@ import org.usfirst.frc.team3414.teleop.Teleop;
 
 public class Diagnostic {
 	static boolean driveEnabled = false;
+	static double speed = 0;
 	public static void checkInput() {
-		double speed = 0;
 		DiagnosticServer.execute();
 		switch(DiagnosticServer.getInput()) {
 		
 		case "exit":
 			System.out.println("Diagnostic Stopped");
+			reset();
 		case "stop":
 			System.out.println("Diagnostic Stopped");
+			reset();
 		case "kill":
 			System.out.println("Killing Robot");
 			System.exit(0);
@@ -74,8 +76,16 @@ public class Diagnostic {
 			return true;
 		}
 	}
+	public static void reset() {
+		speed = 0;
+		DriveTrain.getInstance().left.setFront(0);
+		DriveTrain.getInstance().left.setMiddle(0);
+		DriveTrain.getInstance().left.setRear(0);
+		DriveTrain.getInstance().right.setFront(0);
+		DriveTrain.getInstance().right.setMiddle(0);
+		DriveTrain.getInstance().right.setRear(0);
 
-
+	}
 
 
 
--- src/org/usfirst/frc/team3414/teleop/Controller.java
+++ src/org/usfirst/frc/team3414/teleop/Controller.java
@@ -1,39 +1,52 @@
 package org.usfirst.frc.team3414.teleop;
 
+import org.usfirst.frc.team3414.config.Config;
+
+import edu.wpi.first.wpilibj.Joystick;
+
 public class Controller {
+	Joystick pad = new Joystick(Config.CONTROLLER_CHANNEL);
 	public boolean getAButton() {
+		return pad.getRawButton(1);
+
+	}
+
+	public boolean getBButton() {
+		return pad.getRawButton(2);
+
+	}
+
+	public boolean getXButton() {
 		return false;
-		
-	}
-public boolean getBButton() {
-	return false;
-		
-	}
-public boolean getXButton() {
-	return false;
-	
-}
-public boolean getYButton() {
-	return false;
-	
-}
-public boolean getLBButton() {
-	return false;
-	
-}
-public boolean getRBButton() {
-return false;
-	
-}
-public boolean getLT() {
-return false;
 
-}
-public boolean getRT() {
-return false;
+	}
 
-}
-public double getPov() {
-	return 0;
-}
+	public boolean getYButton() {
+		return false;
+
+	}
+
+	public boolean getLBButton() {
+		return false;
+
+	}
+
+	public boolean getRBButton() {
+		return false;
+
+	}
+
+	public boolean getLT() {
+		return false;
+
+	}
+
+	public boolean getRT() {
+		return false;
+
+	}
+
+	public double getPov() {
+		return 0;
+	}
 }
